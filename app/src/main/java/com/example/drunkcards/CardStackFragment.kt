b3efe71843d7035da2bf4359ed9b7a71d9eb8681
Rawlin Crasto <rawlin@greedygame.com>
2021-03-18 17:29:46 +0530

    code

--- app/src/main/java/com/example/drunkcards/CardStackFragment.kt
+++ app/src/main/java/com/example/drunkcards/CardStackFragment.kt
@@ -2,11 +2,26 @@ package com.example.drunkcards
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.drunkcards.databinding.FragmentCardStackBinding
+
 
 class CardStackFragment : Fragment(R.layout.fragment_card_stack) {
+    private lateinit var binding: FragmentCardStackBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCardStackBinding.bind(view)
+
+
+
+        val cardsStack = mutableListOf<String>("one", "two", "three", "four")
+
+
+
+
 
     }
 }
