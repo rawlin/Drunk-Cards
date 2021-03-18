07ee10fceaacea2e0b2feb4a2a1d2f7b34bedfdc
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-03-18 16:03:37 +0530

    game room created

---
+++ app/src/main/java/com/example/drunkcards/GameRoomFragment.kt
@@ -0,0 +1,13 @@
+package com.example.drunkcards
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class GameRoomFragment :Fragment(R.layout.fragment_game_room){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
