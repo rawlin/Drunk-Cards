5728e51ea8ea78b558624b9fa1d28796d4d7cfba
Rawlin Crasto <rawlin@greedygame.com>
2021-03-18 15:39:40 +0530

    nav graph

---
+++ app/src/main/java/com/example/drunkcards/CardStackFragment.kt
@@ -0,0 +1,12 @@
+package com.example.drunkcards
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class CardStackFragment : Fragment(R.layout.fragment_card_stack) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
