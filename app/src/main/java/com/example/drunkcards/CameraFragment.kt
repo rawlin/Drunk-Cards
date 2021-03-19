a4cba61b0536eaca7112c0ad24f8bed402a8cb3e
Rawlin Crasto <rawlin@greedygame.com>
2021-03-19 11:45:48 +0530

    camera added

--- app/src/main/java/com/example/drunkcards/CameraFragment.kt
+++ app/src/main/java/com/example/drunkcards/CameraFragment.kt
@@ -1,8 +1,100 @@
 package com.example.drunkcards
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.drunkcards.databinding.FragmentCameraBinding
+import timber.log.Timber
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class CameraFragment: Fragment() {
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+    private lateinit var binding: FragmentCameraBinding
+    private lateinit var cameraExecutor: ExecutorService
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCameraBinding.bind(view)
 
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION
+            )
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (requestCode == REQUEST_CODE_PERMISSION) {
+                if (allPermissionsGranted()) {
+                    startCamera()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Permissions not granted by user",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    ActivityCompat.requestPermissions(
+                        requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION
+                    )
+                }
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+            } catch (e: Exception) {
+                Timber.e(e, "Use case binding failed")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSION = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 }
--- app/src/main/java/com/example/drunkcards/GamePlayFragment.kt
+++ app/src/main/java/com/example/drunkcards/GamePlayFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentTransaction
 import com.example.drunkcards.databinding.FragmentGamePlayBinding
 import java.security.Permissions
 
@@ -17,7 +18,13 @@ class GamePlayFragment : Fragment(R.layout.fragment_game_play) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentGamePlayBinding.bind(view)
 
+        setupCameraFragment()
 
+    }
 
+    private fun setupCameraFragment() {
+        val childFragment = CameraFragment()
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.camera_framgent_container, childFragment).commit()
     }
 }
